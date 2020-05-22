b18fee5906adcc40dca38bc4c42dd7bd98c63ef2
Tim Lucas <tim@buildkite.com>
2020-05-22 17:37:16 +1000

    Update bk.rb

--- bk.rb
+++ bk.rb
@@ -3,9 +3,9 @@
 class Bk < Formula
   homepage "https://github.com/buildkite/cli"
   desc "A command line interface for Buildkite."
-  url "$STABLE_ASSET_URL"
+  url "https://github.com/buildkite/cli/releases/download/v1.1.0/bk-darwin-amd64-1.1.0"
   version "v1.1.0"
-  sha256 "$STABLE_ASSET_SHA256"
+  sha256 "580e595c67be43d95ecfd4525c0d0070ea85e8d288370af56ae6d3163c5164f3"
 
   def install
     mv "bk-darwin-amd64-1.1.0", "bk"
